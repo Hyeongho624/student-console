7e2fc402d3df6bd61a9ff20556986f984f784a20
Hyeongho624 <harrypark624@gmail.com>
2026-09-02 14:47:16 +0900

    feat: print student score

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,9 @@ package org.example;
 
 public class Main {
     static void main() {
+        String name = "김하나";
+        int score = 87;
+
+        System.out.println(name + " 학생의 점수는 " + score + "점입니다.");
     }
 }
