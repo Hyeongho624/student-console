ad5f4d69bdefff6efe9aa627576fce2d2dc3746b
Hyeongho624 <harrypark624@gmail.com>
2026-09-02 15:26:19 +0900

    chore: init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,34 +4,5 @@ import java.util.Scanner;
 
 public class Main {
     static void main() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("학생 수를 입력하세요: ");
-        int studentCount = Integer.parseInt(scanner.nextLine());
-        int totalScore = 0;
-
-        for (int i = 0; i <= studentCount; i++) {
-            System.out.println(i + "번째 학생 이름: ");
-            String name = scanner.nextLine();
-
-            System.out.println(i + "번째 학생 점수: ");
-            int score = Integer.parseInt(scanner.nextLine());
-
-            String grade;
-            if (score >= 90) {
-                grade = "A";
-            } else if (score >= 80) {
-                grade = "B";
-            } else if (score >= 70) {
-                grade = "C";
-            } else if (score >= 60) {
-                grade = "D";
-            } else {
-                grade = "F";
-            }
-
-            totalScore = totalScore + score;
-            System.out.println(name + " 학생의 등급은 " + grade + "입니다.");
-        }
     }
 }
